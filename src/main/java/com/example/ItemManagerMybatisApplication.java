688b1574c92990bc202e051fa16c509571ee2dd2
komatsu tomoki <ponjujujujujujuju@gmail.com>
2023-10-09 12:01:06 +0900

    first commit

---
+++ src/main/java/com/example/ItemManagerMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemManagerMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemManagerMybatisApplication.class, args);
+	}
+}
